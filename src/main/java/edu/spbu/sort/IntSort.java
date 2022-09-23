489668963a8bbf5fddd8d63b4a94f397fe382f8f
marin <mar.ina.val.ria@gmail.com>
2022-09-23 13:27:01 +0300

    added quick sort

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,11 +8,34 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
+
+  public static void sort (int[] a, int low, int high){
+    if (a.length == 0) {
+      return;
+    }
+    if (low < high) {
+      int p = (a[low] + a[high])/2;
+      int beg = low;
+      int end = high;
+      while (beg <= end){
+        while(a[beg] < a[p]){
+          ++beg;
+        }
+        while(a[p] <= a[end]){
+          --end;
+        }
+        if (beg <= end) {
+          int h = a[beg];
+          a[beg] = a[end];
+          a[end] = h;
+          ++beg;
+          --end;
+        }
+      }
+      sort(a,low,end);
+      sort(a,beg,high);
+    }
   }
 
-  public static void sort (List<Integer> list) {
-    Collections.sort(list);
-  }
+  public static void sort (List<Integer> list) {Collections.sort(list);}
 }
--- src/test/java/edu/spbu/sort/IntSortTest.java
+++ src/test/java/edu/spbu/sort/IntSortTest.java
@@ -35,7 +35,7 @@ public class IntSortTest
   public void testSortArray() throws Exception {
     int array[] = generateRandomIntArray(ARRAY_SIZE, SEED);
 
-    IntSort.sort(array);
+    IntSort.sort(array,0, array.length);
 
     // проверяем правильность сортировки
     int previousValue = Integer.MIN_VALUE;
